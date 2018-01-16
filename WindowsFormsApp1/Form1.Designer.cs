e8987f70be919d9a8ae36a7e8a5ab2eaad7a2119
samyisok <magaster@gmail.com>
2018-01-16 11:51:55 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.listBoxEx = new System.Windows.Forms.ListBox();
+            this.textBoxEx = new System.Windows.Forms.TextBox();
+            this.addExButton = new System.Windows.Forms.Button();
+            this.DeleteExButton = new System.Windows.Forms.Button();
+            this.listBoxFolders = new System.Windows.Forms.ListBox();
+            this.addFolderButton = new System.Windows.Forms.Button();
+            this.DeleteFolderButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(600, 595);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "";
+            this.button1.Location = new System.Drawing.Point(12, 627);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "To tray!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // listBoxEx
+            // 
+            this.listBoxEx.AccessibleRole = System.Windows.Forms.AccessibleRole.List;
+            this.listBoxEx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBoxEx.FormattingEnabled = true;
+            this.listBoxEx.Location = new System.Drawing.Point(638, 12);
+            this.listBoxEx.Name = "listBoxEx";
+            this.listBoxEx.Size = new System.Drawing.Size(321, 366);
+            this.listBoxEx.TabIndex = 3;
+            this.listBoxEx.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBoxEx
+            // 
+            this.textBoxEx.Location = new System.Drawing.Point(638, 404);
+            this.textBoxEx.Name = "textBoxEx";
+            this.textBoxEx.Size = new System.Drawing.Size(321, 20);
+            this.textBoxEx.TabIndex = 4;
+            // 
+            // addExButton
+            // 
+            this.addExButton.Location = new System.Drawing.Point(638, 448);
+            this.addExButton.Name = "addExButton";
+            this.addExButton.Size = new System.Drawing.Size(145, 23);
+            this.addExButton.TabIndex = 5;
+            this.addExButton.Text = "ADD EX";
+            this.addExButton.UseVisualStyleBackColor = true;
+            this.addExButton.Click += new System.EventHandler(this.addExButton_Click);
+            // 
+            // DeleteExButton
+            // 
+            this.DeleteExButton.Location = new System.Drawing.Point(804, 448);
+            this.DeleteExButton.Name = "DeleteExButton";
+            this.DeleteExButton.Size = new System.Drawing.Size(155, 23);
+            this.DeleteExButton.TabIndex = 6;
+            this.DeleteExButton.Text = "DELETE EX";
+            this.DeleteExButton.UseVisualStyleBackColor = true;
+            this.DeleteExButton.Click += new System.EventHandler(this.DeleteExButton_Click);
+            // 
+            // listBoxFolders
+            // 
+            this.listBoxFolders.FormattingEnabled = true;
+            this.listBoxFolders.Location = new System.Drawing.Point(989, 13);
+            this.listBoxFolders.Name = "listBoxFolders";
+            this.listBoxFolders.Size = new System.Drawing.Size(326, 368);
+            this.listBoxFolders.TabIndex = 7;
+            // 
+            // addFolderButton
+            // 
+            this.addFolderButton.Location = new System.Drawing.Point(989, 400);
+            this.addFolderButton.Name = "addFolderButton";
+            this.addFolderButton.Size = new System.Drawing.Size(147, 23);
+            this.addFolderButton.TabIndex = 8;
+            this.addFolderButton.Text = "Add Folder";
+            this.addFolderButton.UseVisualStyleBackColor = true;
+            this.addFolderButton.Click += new System.EventHandler(this.addFolderButton_Click);
+            // 
+            // DeleteFolderButton
+            // 
+            this.DeleteFolderButton.Location = new System.Drawing.Point(1155, 400);
+            this.DeleteFolderButton.Name = "DeleteFolderButton";
+            this.DeleteFolderButton.Size = new System.Drawing.Size(160, 23);
+            this.DeleteFolderButton.TabIndex = 9;
+            this.DeleteFolderButton.Text = "Delete Folder";
+            this.DeleteFolderButton.UseVisualStyleBackColor = true;
+            this.DeleteFolderButton.Click += new System.EventHandler(this.DeleteFolderButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1327, 671);
+            this.Controls.Add(this.DeleteFolderButton);
+            this.Controls.Add(this.addFolderButton);
+            this.Controls.Add(this.listBoxFolders);
+            this.Controls.Add(this.DeleteExButton);
+            this.Controls.Add(this.addExButton);
+            this.Controls.Add(this.textBoxEx);
+            this.Controls.Add(this.listBoxEx);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.ListBox listBoxEx;
+        private System.Windows.Forms.TextBox textBoxEx;
+        private System.Windows.Forms.Button addExButton;
+        private System.Windows.Forms.Button DeleteExButton;
+        private System.Windows.Forms.ListBox listBoxFolders;
+        private System.Windows.Forms.Button addFolderButton;
+        private System.Windows.Forms.Button DeleteFolderButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public String lastString = "";
+        public String dateTime = "";
+        public List<FileSystemWatcher> watchers = new List<FileSystemWatcher>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void fix( String var, DateTime dateTime)
+        {
+            Console.WriteLine(var);
+            Console.WriteLine("alaram");
+            if (var != null && var != "" && !listBoxEx.Items.Contains(System.IO.Path.GetExtension(var)))
+            {
+                if (lastString.Equals(var)) { //do nothing
+                }
+                else
+                {
+                    textBox1.Text += var + Environment.NewLine;
+                    lastString = var + dateTime.ToShortTimeString();
+                    notifyIcon1_balloon(var, "Update ");
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            this.Hide();
+        }
+        /* 
+         private void notifyIcon1_Click(object sender, EventArgs e)
+         {
+             this.Show();
+             this.WindowState = FormWindowState.Normal;
+         }
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Text = "AraMonitoring";
+            // load ex settings.
+            if (AraMonitoring.Properties.Settings.Default.ex != "")
+            {
+                string exParams = AraMonitoring.Properties.Settings.Default.ex;
+                listBoxEx.Items.AddRange(JsonConvert.DeserializeObject<Object[]>(exParams));
+            }
+            // load settings settings.
+            if (AraMonitoring.Properties.Settings.Default.folders != "")
+            {
+                string foldersParams = AraMonitoring.Properties.Settings.Default.folders;
+                listBoxFolders.Items.AddRange(JsonConvert.DeserializeObject<Object[]>(foldersParams));
+            }
+
+            foreach (string path in listBoxFolders.Items)
+            {
+                String watchFolder = path;
+                watchers.Add(new FileSystemWatcher(watchFolder));
+                System.Console.WriteLine("create wathcer " + watchFolder);
+            };
+
+            foreach (FileSystemWatcher fsw in watchers) { 
+                System.Console.WriteLine("create fsw " + fsw.Path);
+                fsw.IncludeSubdirectories = true;
+                //fsw.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName;
+                fsw.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                    | NotifyFilters.FileName | NotifyFilters.DirectoryName
+                    | NotifyFilters.Size | NotifyFilters.CreationTime
+                    | NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.Security;
+                fsw.Created += new FileSystemEventHandler(fsw_Changed);
+                fsw.EnableRaisingEvents = true;
+            }
+        }
+
+        void fsw_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (textBox1.InvokeRequired)
+                textBox1.Invoke((MethodInvoker)delegate ()
+                {
+                    System.Console.WriteLine(e.FullPath);
+                    fix(e.FullPath, DateTime.Now);
+                });
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+
+        private void notifyIcon1_balloon(String text, String title)
+        {
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = text;
+            notifyIcon1.BalloonTipTitle = title;
+            notifyIcon1.ShowBalloonTip(500);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addExButton_Click(object sender, EventArgs e)
+        {
+            if ( textBoxEx.Text != null )
+            {
+                String stringEx = textBoxEx.Text;
+                if (stringEx.Equals("") || listBoxEx.Items.Contains(stringEx))
+                {
+                    //do nothing
+                }
+                else
+                {
+                    listBoxEx.Items.Add(stringEx);
+                    ListBox.ObjectCollection myItems = listBoxEx.Items;
+                    string output = JsonConvert.SerializeObject(myItems);
+                    AraMonitoring.Properties.Settings.Default.ex = output;
+                    AraMonitoring.Properties.Settings.Default.Save();
+                    System.Console.WriteLine(output);
+                }
+            }
+        }
+
+        private void DeleteExButton_Click(object sender, EventArgs e)
+        {
+            listBoxEx.Items.Remove(listBoxEx.SelectedItem);
+            ListBox.ObjectCollection myItems = listBoxEx.Items;
+            string output = JsonConvert.SerializeObject(myItems);
+            AraMonitoring.Properties.Settings.Default.ex = output;
+            AraMonitoring.Properties.Settings.Default.Save();
+            System.Console.WriteLine(output);
+
+        }
+
+        private void addFolderButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.Description = "Select folder to monitoring";
+            fbd.ShowNewFolderButton = true;
+            if (fbd.ShowDialog() == DialogResult.OK && !listBoxFolders.Items.Contains(fbd.SelectedPath) )
+            {
+                listBoxFolders.Items.Add(fbd.SelectedPath);
+                ListBox.ObjectCollection myItems = listBoxFolders.Items;
+                string output = JsonConvert.SerializeObject(myItems);
+                AraMonitoring.Properties.Settings.Default.folders = output;
+                AraMonitoring.Properties.Settings.Default.Save();
+                System.Console.WriteLine(output);
+
+            }
+        }
+
+        private void DeleteFolderButton_Click(object sender, EventArgs e)
+        {
+            listBoxFolders.Items.Remove(listBoxFolders.SelectedItem);
+            ListBox.ObjectCollection myItems = listBoxFolders.Items;
+            string output = JsonConvert.SerializeObject(myItems);
+            AraMonitoring.Properties.Settings.Default.folders = output;
+            AraMonitoring.Properties.Settings.Default.Save();
+            System.Console.WriteLine(output);
+        }
+    }
+}
